cdab9a36ad82336d395810079e04f704b3ea6cda
Faith Jubilee <jubileefaith36@gmail.com>
2023-02-08 23:51:44 +0100

    Created a color picker app

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -9,28 +9,29 @@
 </template>
 
 <script>
-import { ref, reactive } from "@vue/reactivity";
-
+import composablefile from "../composables/composablefile"
 export default {
-  setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
-
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
-
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
+  name: "ColorPickerGame",
+  data() {
+    return {
+      colors: [],
+      message: "",
     };
-
+  },
+  methods: {
+    matchColor() {},
+  },
+  setup() {
+    const { colors, message, matchColor } = composablefile();
     return { colors, message, matchColor };
   },
 };
 </script>
 
-<style></style>
+<style>
+  button {
+    padding: 10px 20px;
+    margin: 10px;
+    border-radius: 5px;
+  }
+</style>
